4894281ec92faa1f23be8eb591cb90462790b4bf
Mukhammad Pardaboev <mukhammad.lb.96@gmail.com>
2021-02-28 13:07:08 +0500

    added unit tests

---
+++ 22-topshiriq_Unit_test/myLib.js
@@ -0,0 +1,4 @@
+// Test numbers - absolute value
+module.exports.absolute = function (number) {
+    return (number >= 0) ? number : -number;
+}
---
+++ 22-topshiriq_Unit_test/tests/mylib.test.js
@@ -0,0 +1,19 @@
+const myLib = require('../myLib');
+
+describe('absolute', () => {
+
+    it('should return a positive number if input is positive', () => {
+        const result = myLib.absolute(1);
+        expect(result).toBe(1);
+    });
+
+    it('should return a positive number if input is negative', () => {
+        const result = myLib.absolute(-1);
+        expect(result).toBe(1);
+    });
+
+    it('should return 0 if input is 0', () => {
+        const result = myLib.absolute(0);
+        expect(result).toBe(0);
+    });
+});
